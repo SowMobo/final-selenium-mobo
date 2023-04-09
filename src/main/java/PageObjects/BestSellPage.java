cc8603c5edd196aa1945f086b50cb105596f5ee1
SowModibo <sowmodibo62@gmail.com>
2023-04-09 15:13:42 +0200

    highTechPageBy locator modified

--- src/main/java/PageObjects/BestSellPage.java
+++ src/main/java/PageObjects/BestSellPage.java
@@ -13,15 +13,15 @@ public class BestSellPage {
 
     WebDriver driver;
     WebDriverWait wait;
-    By highTechBy = By.cssSelector("._p13n-zg-nav-tree-all_style_zg-browse-height-small__nleKL>a");
-
+    //._p13n-zg-nav-tree-all_style_zg-browse-height-small__nleKL>a
+    By highTechPageBy = By.cssSelector("#zg_left_col2 a");
     //#zg_left_col2 div[role='treeitem']
     public BestSellPage(WebDriver driver) {
         this.driver = driver;
         wait = new WebDriverWait(driver, Duration.ofSeconds(TIMEOUT));
     }
     private void openPageBy(int index) {
-        wait.until(ExpectedConditions.presenceOfAllElementsLocatedBy(highTechBy))
+        wait.until(ExpectedConditions.presenceOfAllElementsLocatedBy(highTechPageBy))
                 .get(index).click();
     }
 
